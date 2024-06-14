7be30b143d710e386f65c927145f8708e7150ad2
Benedict Payot <159107303+benedictpayot@users.noreply.github.com>
2024-06-14 21:29:26 +0800

    Turn choice conversion into a function and create playRound function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let computerScore = 0;
 let humanScore = 0;
 
+function choiceConvert(stringToConvert) {
+    let formattedString = stringToConvert.toLowerCase().replace(/[^a-z]/g, "");
+
+    return formattedString;
+}
+
 function getComputerChoice() {
     let choice = Math.round(Math.random() * 100) / 100;
     let computerPick;
@@ -13,13 +19,41 @@ function getComputerChoice() {
     else {
         computerPick = "Scissors";
     }
-    return computerPick;
+
+    let formattedComputerPick = choiceConvert(computerPick);
+    return formattedComputerPick;
 }
 console.log(getComputerChoice());
 
 function getHumanChoice($choice) {
     let choice = prompt("Choose between Rock, Paper, or Scissors");
-    let formattedChoice = choice.toLowerCase().replace(/[^a-z]/g, "");
+    let formattedChoice = choiceConvert(choice);
     console.log(formattedChoice);
     return formattedChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if(humanSelection === "rock" && computerSelection === "paper") {
+        console.log("You lose! " + computerSelection + " beats " + humanSelection + "!");
+    }
+    else if(humanSelection === "paper" && computerSelection === "scissors") {
+        console.log("You lose! " + computerSelection + " beats " + humanSelection + "!");
+    }
+    else if(humanSelection === "scissors" && computerSelection === "rock") {
+        console.log("You lose! " + computerSelection + " beats " + humanSelection + "!");
+    }
+    else if(humanSelection === "rock" && computerSelection === "scissors") {
+        console.log("You win! " + humanSelection + " beats " + computerSelection + "!");
+    }
+    else if(humanSelection === "paper" && computerSelection === "rock") {
+        console.log("You win! " + humanSelection + " beats " + computerSelection + "!");
+    }
+    else if(humanSelection === "scissors" && computerSelection === "paper") {
+        console.log("You win! " + humanSelection + " beats " + computerSelection + "!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
