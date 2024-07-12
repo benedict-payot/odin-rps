369520dcf50587a3e2ec0837d4fa9f62a5d28c7c
Benedict Payot <159107303+benedictpayot@users.noreply.github.com>
2024-07-12 20:47:38 +0800

    Redo whole script file to fix runtime errors on playGame function

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function choiceConvert(stringToConvert) {
     let formattedString = stringToConvert.toLowerCase().replace(/[^a-z]/g, "");
-
     return formattedString;
 }
 
 function getComputerChoice() {
-    let choice = Math.round(Math.random() * 100) / 100;
+    let choice = Math.random();
     let computerPick;
     if(choice <= 0.33) {
         computerPick = "rock";
@@ -16,104 +15,83 @@ function getComputerChoice() {
     else {
         computerPick = "scissors";
     }
-
-    let formattedComputerPick = choiceConvert(computerPick);
-    return formattedComputerPick;
+    return choiceConvert(computerPick);
 }
-console.log(getComputerChoice());
 
-function getHumanChoice($choice) {
+function getHumanChoice() {
     let choice = prompt("Choose between Rock, Paper, or Scissors");
-    // if(choice === null) {
-    //     return;
-    // }
-    let formattedChoice = choiceConvert(choice);
-    return formattedChoice;
+    if(choice === null) {
+        return;
+    }
+    return choiceConvert(choice);
+}
+
+function playRound(humanSelection, computerSelection) {
+    if(computerSelection === "rock") {
+        if(humanSelection === "paper") {
+            return "win";
+        }
+        else if (humanSelection === "scissors") {
+            return "lose";
+        }
+        else {
+            return "tie";
+        }
+    }
+    else if (computerSelection === "paper") {
+        if(humanSelection === "rock") {
+            return "lose";
+        }
+        else if (humanSelection === "scissors") {
+            return "win";
+        }
+        else {
+            return "tie";
+        }
+    }
+    else if (computerSelection === "scissors") {
+        if(humanSelection === "paper") {
+            return "lose";
+        }
+        else if (humanSelection === "rock") {
+            return "win";
+        }
+        else {
+            return "tie";
+        }
+    }
+    return "invalid";
 }
 
 function playGame() {
-
     let computerScore = 0;
     let humanScore = 0;
-
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    let result;
-    let roundResult;
-    let gameResult;
     const pointsRequired = 5;
-    let gameOver = "false";
 
-    function playRound(humanChoice, computerChoice) {
-        if(humanSelection === "rock") {
-            if(computerSelection === "scissors") {
-                result = "win";
-            }
-            else if (computerSelection === "paper") {
-                result = "lose";
-            }
-            else {
-                result = "tie";
-                console.log("It's a tie!");
-            }
+    while(humanScore < pointsRequired && computerScore < pointsRequired) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(humanSelection, computerSelection);
+
+        console.log(`Human: ${humanSelection}, Computer: ${computerSelection}, Result: ${roundResult}`);
+
+        if(roundResult === "win") {
+            humanScore++;
         }
-        else if (humanSelection === "paper") {
-            if(computerSelection === "rock") {
-                result = "win";
-            }
-            else if (computerSelection === "scissors") {
-                result = "lose";
-            }
-            else {
-                result = "tie";
-                console.log("It's a tie!");
-            }
-        }
-        else if (humanSelection === "scissors") {
-            if(computerSelection === "paper") {
-                result = "win";
-            }
-            else if (computerSelection === "rock") {
-                result = "lose";
-            }
-            else {
-                result = "tie";
-                console.log("It's a tie!");
-            }
-        }
-        else {
-            result = "invalid";
+        else if(roundResult === "lose") {
+            computerScore++;
         }
 
-        return result;
+        console.log(`Scores - Human: ${humanScore}, Computer: ${computerScore}`);
     }
 
-    if(humanScore === pointsRequired || computerScore === pointsRequired) {
+    if(humanScore === pointsRequired) {
+        console.log("Human wins the game!");
+        return;
+    } else if(computerScore === pointsRequired) {
+        console.log("Computer wins the game!");
         return;
     }
-
-    for(i = 0; i<=pointsRequired; i++) {
-        roundResult = playRound(humanSelection, computerSelection);
-    }
-
-    if(roundResult === "win") {
-        humanScore++;
-        if(humanScore === pointsRequired) {
-            gameResult = "win";
-            gameOver = true;
-        }
-    }
-    else if(roundResult === "lose") {
-        computerScore++;
-        if(computerScore === pointsRequired) {
-            gameResult = "lose";
-            gameOver = true;
-        }
-    }
-
-    console.log(roundResult);
-    
 }
 
 playGame();
